e88e245ba5509bdc07b7d843df1b8237cb722151
Osomizu <jae.dreijlingg@outlook.com>
2024-06-01 13:47:16 +0200

    extends for enemy

---
+++ Project/OOPD-Beroepsproduct/src/main/java/org/example/entitys/enemys2/Enemy.java
@@ -0,0 +1,41 @@
+package org.example.entitys.enemys2;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Collided;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
+import org.example.entitys.blackpixelcat.BlackPixelCat;
+import org.example.scenes.levels.DonkerebosScene;
+
+import java.util.List;
+
+public abstract class Enemy extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Collided, Collider {
+    protected BlackPixelCat blackPixelCat;
+    protected DonkerebosScene donkerebosScene;
+
+    public Enemy(String resource, Coordinate2D initialLocation, BlackPixelCat blackPixelCat, DonkerebosScene donkerebosScene) {
+        super(resource, initialLocation);
+        this.blackPixelCat = blackPixelCat;
+        this.donkerebosScene = donkerebosScene;
+    }
+    @Override
+    public void notifyBoundaryCrossing(SceneBorder border){
+        changeDirection(180);
+    };
+
+    @Override
+    public void onCollision(final List<Collider> collidingObjects) {
+        for (var collider : collidingObjects) {
+            if (collider instanceof BlackPixelCat) {
+                hit();
+                donkerebosScene.checkForWinOrLose();
+                donkerebosScene.updateHealthText();
+                donkerebosScene.setBlackPixelCatRandom();
+            }
+        }
+    }
+    public abstract void hit();
+}
---
+++ Project/OOPD-Beroepsproduct/src/main/java/org/example/entitys/enemys2/Rat.java
@@ -0,0 +1,26 @@
+package org.example.entitys.enemys2;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Direction;
+import org.example.entitys.blackpixelcat.BlackPixelCat;
+import org.example.scenes.levels.DonkerebosScene;
+
+public class Rat extends Enemy{
+    private static final String RESOURCE = "entitys/enemys/RATATA.png";
+    private static final int DAMAGE_TO_PLAYER = 2;
+    private static final int SPEED = 2;
+    private static final Direction DIRECTION = Direction.valueOf(270d);
+
+    public Rat(Coordinate2D initialLocation, BlackPixelCat blackPixelCat, DonkerebosScene donkerebosScene) {
+        super(RESOURCE, initialLocation, blackPixelCat, donkerebosScene);
+        setMotion(SPEED, DIRECTION);
+    }
+
+    @Override
+    public void hit() {
+        int newHealth = blackPixelCat.getHealth() - DAMAGE_TO_PLAYER;
+        blackPixelCat.setHealth(newHealth);
+    }
+
+
+}
